d24f7b9bfdd79dbda9687c3f2837a7565923afe8
Khánh Nhi <nhi.tuong25@student.passerellesnumeriques.org>
2024-06-14 16:44:07 +0700

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-import pn_logo from "./assets/pn_logo.png";
-
-function Header() {
+import pn_logo from "./assets/pn_logo.png"; 
+function Header({ batchName }) { 
   return (
     <>
       <header id="header">
         <img src={pn_logo} alt="PN Logo" />
-        <h1>Students results for {batchName} </h1>
+        <h1>Students results for {batchName}</h1>
       </header>
     </>
   );
 }
+
 export default Header;
